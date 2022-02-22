9f2507810f13b5fc12c92048188b35ea4439f1ac
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-21 21:33:26 -0600

    Adds logic to confirm a package pickup

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] float destroySpeed = 0.5f;
+    [SerializeField] Color32 enrouteColor = new Color32(25, 255, 1, 255);
+    [SerializeField] Color32 notEnrouteColor = new Color32(226, 199, 1, 255);
+
+    private SpriteRenderer spriteRenderer;
 
     private bool enroute = false;
     private bool delivered;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("I'm hit!");
@@ -17,6 +25,7 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package" && !enroute) {
             enroute = true;
             delivered = false;
+            spriteRenderer.color = enrouteColor;
             Destroy(other.gameObject, destroySpeed);
             Debug.Log("Package Picked Up");
         }
@@ -24,6 +33,7 @@ public class Delivery : MonoBehaviour
         if(other.tag == "DeliveryDestination" && enroute) {
             enroute = false;
             delivered = true;
+            spriteRenderer.color = notEnrouteColor;
             Debug.Log("Package Delivered");
         }
     }
