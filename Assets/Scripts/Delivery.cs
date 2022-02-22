01cc3b445e097a6889d11a2f510166119acd8288
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-21 21:06:55 -0600

    Adds logic to clear packages on pickup

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroySpeed = 0.5f;
+
+    private bool enroute = false;
+    private bool delivered;
+
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("I'm hit!");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Package") {
+        if(other.tag == "Package" && !enroute) {
+            enroute = true;
+            delivered = false;
+            Destroy(other.gameObject, destroySpeed);
             Debug.Log("Package Picked Up");
         }
 
-        if(other.tag == "DeliveryDestination") {
+        if(other.tag == "DeliveryDestination" && enroute) {
+            enroute = false;
+            delivered = true;
             Debug.Log("Package Delivered");
         }
     }
